4b8ca34c5c799d73b5b2d1663c18ff209b78716e
yyaannnn <yannick.fayemi@gmail.com>
2023-04-07 10:00:01 +0200

    Update TD_GitHub.cpp

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,12 +1,35 @@
 //Fonctions réalisées par l'étudiant 1 :
+#include <cmath>
+#include <iostream>
+using namespace std;
 
+bool estMajeur(int age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
+int exposant(nombre, exposant) {
+  return pow(nombre, exposant);
+}
 
+float TVA(prix) {
+  tauxTVA = 0.196; // taux de TVA à 19,6%
+  return prix * (1 + tauxTVA);
+}
 
-
-
-
-
+int plusGrand(a, b, c) {
+  max = a;
+  if (b > max) {
+    max = b;
+  }
+  if (c > max) {
+    max = c;
+  }
+  return max;
+}
 
 
 //Fonctions réalisées par l'étudiant 2 :
